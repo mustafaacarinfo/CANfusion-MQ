8a4bb63a29d6eee36dbffa943afc35154c4603e9
mustafaacarinfo <mustafaacariletisim@gmail.com>
2025-07-16 11:03:02 +0300

    toolchain has been set up to arm7.

--- build.sh
+++ build.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 set -e
 
-# build klasörü yoksa yarat
-[ -d build ] || mkdir build
-cd build
+BUILD_DIR=build
 
-# cmake: native build, toolchain yok
-cmake .. -DCMAKE_BUILD_TYPE=Release
+if [ "$1" = "pi" ]; then
+  echo ">>> Pi için cross-compile"
+  cmake_args="-DBUILD_FOR_PI=ON"
+else
+  echo ">>> Host için native build"
+  cmake_args=""
+fi
 
-# derle
+rm -rf $BUILD_DIR
+mkdir -p $BUILD_DIR && cd $BUILD_DIR
+
+cmake .. -DCMAKE_BUILD_TYPE=Release $cmake_args
 cmake --build . -j$(nproc)
--- toolchains/raspi.cmake
+++ toolchains/raspi.cmake
@@ -1,40 +1,22 @@
 # toolchains/raspi.cmake
 
-# 1) Tanım: Bu bir cross‐build, hedef ARM/Pi
-set(CMAKE_SYSTEM_NAME       Linux)
-set(CMAKE_SYSTEM_PROCESSOR  arm)
+set(CMAKE_SYSTEM_NAME    Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_SYSROOT        /opt/rpi-sysroot)
+set(CMAKE_C_COMPILER   /opt/linaro-buster/bin/arm-linux-gnueabihf-gcc)
+set(CMAKE_CXX_COMPILER /opt/linaro-buster/bin/arm-linux-gnueabihf-g++)
 
-# 2) Kompilerler (tam yol verin)
-set(CMAKE_C_COMPILER        /usr/bin/arm-linux-gnueabihf-gcc)
-set(CMAKE_CXX_COMPILER      /usr/bin/arm-linux-gnueabihf-g++)
-
-# 3) Sysroot (Pi rootfs)
-set(CMAKE_SYSROOT           /opt/rpi-sysroot)
-
-# after set(CMAKE_SYSROOT …)
-set(CMAKE_EXE_LINKER_FLAGS  
-    "--sysroot=${CMAKE_SYSROOT} -L${CMAKE_SYSROOT}/usr/lib ${CMAKE_EXE_LINKER_FLAGS}"
-)
-
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-# 4) Derleme bayrakları → mutlaka --sysroot
-set(CMAKE_C_FLAGS           "--sysroot=${CMAKE_SYSROOT} ${CMAKE_C_FLAGS}")
-set(CMAKE_CXX_FLAGS         "--sysroot=${CMAKE_SYSROOT} ${CMAKE_CXX_FLAGS}")
-
-# 5) Link bayrakları → mutlaka --sysroot + doğru kütüphane dizini
-set(CMAKE_EXE_LINKER_FLAGS  "--sysroot=${CMAKE_SYSROOT} -L${CMAKE_SYSROOT}/usr/lib ${CMAKE_EXE_LINKER_FLAGS}")
-set(CMAKE_SHARED_LINKER_FLAGS "--sysroot=${CMAKE_SYSROOT} -L${CMAKE_SYSROOT}/usr/lib ${CMAKE_SHARED_LINKER_FLAGS}")
-
-link_directories(${CMAKE_SYSROOT}/usr/lib
-                 ${CMAKE_SYSROOT}/usr/lib/arm-linux-gnueabihf)
-
+set(CMAKE_C_FLAGS        "--sysroot=${CMAKE_SYSROOT} ${CMAKE_C_FLAGS}")
+set(CMAKE_CXX_FLAGS      "--sysroot=${CMAKE_SYSROOT} ${CMAKE_CXX_FLAGS}")
 set(CMAKE_EXE_LINKER_FLAGS  "--sysroot=${CMAKE_SYSROOT}")
 set(CMAKE_SHARED_LINKER_FLAGS "--sysroot=${CMAKE_SYSROOT}")
-set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 
-# 6) Bulma modları → kesinlikle yalnız sysroot
-set(CMAKE_FIND_ROOT_PATH    ${CMAKE_SYSROOT})
+set(CMAKE_FIND_ROOT_PATH            ${CMAKE_SYSROOT})
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
+
+set(ENV{PKG_CONFIG_SYSROOT_DIR}   "${CMAKE_SYSROOT}")
+# PKG_CONFIG_LIBDIR içinde iki dizini “:” ile ayırın
+set(ENV{PKG_CONFIG_LIBDIR}        "${CMAKE_SYSROOT}/usr/lib/pkgconfig:${CMAKE_SYSROOT}/usr/share/pkgconfig")
